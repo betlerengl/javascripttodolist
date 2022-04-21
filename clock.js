b72b2e8d871d1e5ba74dac85e10209c128413216
Betül Erengül <96680104+betlerengl@users.noreply.github.com>
2022-04-21 11:58:45 +0300

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,46 @@
+let nameUser = prompt("Enter The Your Name");
+nameUser = nameUser.charAt(0).toLocaleUpperCase() +nameUser.slice(1);
+// console.log(nameUser)
+let user = document.querySelector("#myName")
+
+if (nameUser.length > 0) {
+    user.innerHTML = `${nameUser}`;
+
+} else {
+    alert("Enter Your Name!")
+}
+
+function currentTime() {
+    // Days 
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    const d = new Date();
+    let day = weekday[d.getDay()];
+    
+
+
+   let date = new Date();
+
+   let hh = date.getHours();
+   let mm = date.getMinutes();
+   let ss = date.getSeconds();
+   let session = "AM";
+
+   if(hh > 12){
+       session = "PM";
+   }
+
+   hh = (hh < 10) ? "0" + hh : hh;
+   mm = (mm < 10) ? "0" + mm : mm;
+   ss = (ss < 10) ? "0" + ss : ss;
+
+   let time = hh + ":" + mm + ":" + ss + " " + session;
+
+   document.querySelector("#myClock").innerHTML = `  ${hh}:${mm}:${ss} ${session} ${weekday[d.getDay()]} `;
+   var t = setTimeout(function(){ currentTime() }, 1000);
+   
+
+
+}
+
+currentTime();
